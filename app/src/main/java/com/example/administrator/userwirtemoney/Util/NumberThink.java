ff50e5753d04760a08d36088c49ecca6aa969aa4
Jam <1143241513@qq.com>
2017-03-15 16:15:17 +0800

    finished ThinkWriteMoeny

---
+++ app/src/main/java/com/example/administrator/userwirtemoney/Util/NumberThink.java
@@ -0,0 +1,177 @@
+package com.example.administrator.userwirtemoney.Util;
+
+import android.view.View;
+import android.widget.TextView;
+import com.example.administrator.userwirtemoney.R;
+
+
+/**
+ * Created by Administrator on 2017/3/15 0015.
+ */
+
+public class NumberThink {
+
+    public static final long MAX_INFO = 99999999;
+    public static boolean DIAN_FALG = true;
+    public static int index = 1;
+    public static String date_info;
+
+    public static long send(View v, long number_info, TextView numberText){
+        long result = 0;
+        switch (v.getId()){
+            case R.id.number_one:
+                if(DIAN_FALG){
+                    result = setNumber(1,number_info,numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(1,number_info,numberText);
+                }
+                break;
+            case R.id.number_two:
+                if(DIAN_FALG) {
+                    result = setNumber(2, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(2,number_info,numberText);
+                }
+                break;
+            case R.id.number_three:
+                if(DIAN_FALG) {
+                    result = setNumber(3, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(3,number_info,numberText);
+                }
+                break;
+            case R.id.number_four:
+                if(DIAN_FALG) {
+                    result = setNumber(4, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(4,number_info,numberText);
+                }
+                break;
+            case R.id.number_fie:
+                if(DIAN_FALG) {
+                    result = setNumber(5, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(5,number_info,numberText);
+                }
+                break;
+            case R.id.number_six:
+                if(DIAN_FALG) {
+                    result = setNumber(6, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(6,number_info,numberText);
+                }
+                break;
+            case R.id.number_seven:
+                if(DIAN_FALG) {
+                    result = setNumber(7, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(7,number_info,numberText);
+                }
+                break;
+            case R.id.number_eight:
+                if(DIAN_FALG) {
+                    result = setNumber(8, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(8,number_info,numberText);
+                }
+                break;
+            case R.id.number_line:
+                if(DIAN_FALG) {
+                    result = setNumber(9, number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(9,number_info,numberText);
+                }
+                break;
+            case R.id.number_zero:
+                if(DIAN_FALG) {
+                    result = onClickToZero(number_info, numberText);
+                }else if(!DIAN_FALG&&index<=2){
+                    result = onClickToDian(0,number_info,numberText);
+                }
+                break;
+            case R.id.number_dian:
+                if(number_info>0&&DIAN_FALG){
+                    DIAN_FALG = false;
+                    date_info = number_info+".";
+                    numberText.setText(date_info);
+                }else if(!DIAN_FALG){
+                    result = number_info;
+                }
+                break;
+            case R.id.back_btn:
+                result = delNumber(numberText);
+                break;
+
+        }
+        return result;
+    }
+
+    public static long setNumber(long info,long number_info,TextView numberText){
+
+        if(number_info==0){
+            number_info+=info;
+            numberText.setText(number_info+"");
+        }else{
+            number_info*=10;
+            number_info+=info;
+            if(number_info<=MAX_INFO){
+                numberText.setText(number_info+"");
+            }else{
+                number_info-=info;
+                number_info/=10;
+            }
+        }
+        return number_info;
+    }
+
+    public static long onClickToZero(long number_info,TextView numberText){
+        if(number_info*10<=MAX_INFO){
+            numberText.setText(number_info*10+"");
+            return number_info*10;
+        }
+        return number_info;
+    }
+
+    public static long onClickToDian(long info,long number_info,TextView numberText){
+        if(info==0){
+            if(index==1){
+                date_info+="0";
+                numberText.setText(date_info);
+                index++;
+            }else if(index==2){
+                if(number_info==0){
+                    date_info+="0";
+                    numberText.setText(date_info);
+                    index++;
+                }else if(number_info>0){
+                    number_info*=10;
+                    numberText.setText(date_info+number_info);
+                    index++;
+                }
+            }
+        }else if(number_info==0){
+            number_info+=info;
+            numberText.setText(date_info+number_info);
+            index++;
+        }else{
+            number_info*=10;
+            number_info+=info;
+            numberText.setText(date_info+number_info);
+            index++;
+        }
+        return number_info;
+    }
+
+    public static long delNumber(TextView numberText){
+        long numberInfo = 0;
+        if(DIAN_FALG){
+            numberInfo = Long.parseLong(numberText.getText().toString());
+            numberInfo/=10;
+            numberText.setText(numberInfo+"");
+        }else{
+
+        }
+        return numberInfo;
+    }
+
+}
--- app/src/main/java/com/example/administrator/userwirtemoney/WriteMoneyActivity.java
+++ app/src/main/java/com/example/administrator/userwirtemoney/WriteMoneyActivity.java
@@ -3,15 +3,60 @@ package com.example.administrator.userwirtemoney;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.administrator.userwirtemoney.Util.NumberThink;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/3/14 0014.
  */
 
-public class WriteMoneyActivity extends AppCompatActivity {
+public class WriteMoneyActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public List<Button> buttons_number;
+    public RelativeLayout backLayout;
+    public TextView numberText ;
+    public long number_info =0;
+    public int count = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_money);
+        buttons_number = new ArrayList<>();
+        iniDate();
+
     }
+    public void iniDate(){
+        buttons_number.add((Button) findViewById(R.id.number_one));
+        buttons_number.add((Button) findViewById(R.id.number_two));
+        buttons_number.add((Button) findViewById(R.id.number_three));
+        buttons_number.add((Button) findViewById(R.id.number_four));
+        buttons_number.add((Button) findViewById(R.id.number_fie));
+        buttons_number.add((Button) findViewById(R.id.number_six));
+        buttons_number.add((Button) findViewById(R.id.number_seven));
+        buttons_number.add((Button) findViewById(R.id.number_eight));
+        buttons_number.add((Button) findViewById(R.id.number_line));
+        buttons_number.add((Button) findViewById(R.id.number_zero));
+        buttons_number.add((Button) findViewById(R.id.number_dian));
+        numberText = (TextView) findViewById(R.id.number_text);
+        backLayout = (RelativeLayout) findViewById(R.id.back_btn);
+        for(Button button:buttons_number){
+            button.setOnClickListener(WriteMoneyActivity.this);
+        }
+        backLayout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+       number_info = NumberThink.send(v,number_info,numberText);
+    }
+
+
 }
